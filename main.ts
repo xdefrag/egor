440599d35aec550a601955d16b9a88a0e92a977d
Stanislav Karkavin <me@xdefrag.dev>
2024-08-04 19:45:18 +0200

    feat: add feedback support

--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
+import Logger from "https://deno.land/x/logger@v1.1.6/logger.ts";
 import { Telegraf } from "npm:telegraf";
 
 const token = Deno.env.get("EGOR_TOKEN") || "";
+const targetChatId = Deno.env.get("TARGET_CHAT_ID") || 0;
+
+const l = new Logger();
 const bot = new Telegraf(token);
 
-bot.hears("/start", (ctx) => {
+bot.use((ctx, next) => {
+  l.info("[msg]", ctx.from?.username, ctx.chat?.id, ":", ctx.text);
+  return next();
+});
+
+bot.start((ctx) => {
   ctx.reply("Hello World");
 });
 
+bot.on("message", (ctx) => {
+  if (ctx.text === "" || ctx.chat.type === "supergroup") return;
+
+  ctx.forwardMessage(targetChatId);
+  ctx.reply("Thanks. Wait for contact");
+});
+
 bot.launch();
+
+Deno.addSignalListener("SIGINT", () => bot.stop());
+Deno.addSignalListener("SIGTERM", () => bot.stop());
+
+l.info("[egor] started");
